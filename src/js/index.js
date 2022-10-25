2eda36c8ccb87796da14b0157fd055c5032e7833
akhvatov <superbus414@gmail.com>
2022-10-25 10:19:42 +0300

    minor fixes
    Добавление JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 import '../scss/style.scss';
 
-console.log('Works!');
+let brandButton = document.querySelector('.section-button_brand');
+let typesButton = document.querySelector('.section-button_types');
+let wrapperActiveBrand = document.querySelector('.swiper__wrapper_brand');
+let wrapperActiveTypes = document.querySelector('.swiper__wrapper_types');
+
+let showMoreBrand = () => {
+    wrapperActiveBrand.classList.toggle('swiper__wrapper_active');
+    console.log(wrapperActive);
+}
+
+let showMoreType = () => {
+    wrapperActiveTypes.classList.toggle('swiper__wrapper_active');
+}
+
+brandButton.addEventListener('click', showMoreBrand);
+typesButton.addEventListener('click', showMoreType);
